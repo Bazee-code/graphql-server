db2a9ae82869791b40764b451ccf90fac4f4c51b
Bazee-code <eugeneobazee2@gmail.com>
2022-04-08 17:13:30 +0300

    Pass schema to middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 // create app
 const app = express();
 
 // middleware -> funcs that have access to the req & res objects and next()
 // next() help us to end the req-res cycle avoiding leaving our req hanging
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema }));
 
 // listen to port
 app.listen(4000, () => {
