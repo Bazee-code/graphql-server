d4e28a6f09e4ed4278093aacfd93000351a30236
Bazee-code <eugeneobazee2@gmail.com>
2022-04-08 17:39:23 +0300

    Change id type from graphQlString to graphQlID

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 // schema -> define object types & relations btwn obj types
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
 let books = [
@@ -26,7 +26,7 @@ let books = [
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -38,7 +38,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } }, //arguments to our query i.e book('123'){name,author}
+      args: { id: { type: GraphQLID } }, //arguments to our query i.e book('123'){name,author}
       resolve(parent, args) {
         // code to get data from db
         // parent -> relationships btwn data
