b35ed96ff652d939db303cfc93f30bc0d06a3d72
Bazee-code <eugeneobazee2@gmail.com>
2022-04-08 16:46:24 +0300

    Create express-graphql middleware

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 // create app
 const app = express();
+
+// middleware -> funcs that have access to the req & res objects and next()
+// next() help us to end the req-res cycle avoiding leaving our req hanging
+app.use('/graphql', graphqlHTTP({}));
 
 // listen to port
 app.listen(4000, () => {
